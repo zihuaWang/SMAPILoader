d6506e6ed7db3612adefea3a0b3ac2ada5d660e8
NRTnarathip <61742272+NRTnarathip@users.noreply.github.com>
2024-11-07 13:12:03 +0700

    Add project files.

---
+++ SMAPILoaderDLL/Program.cs
@@ -0,0 +1,10 @@
+namespace SMAPILoaderDLL;
+
+public static class Program
+{
+
+    public static void Start()
+    {
+        Android.Util.Log.Debug("SMAPI-Tag", "Starting SMAPI Loader DLL...");
+    }
+}
---
+++ SMAPILoaderDLL/post-build.cmd
@@ -0,0 +1,7 @@
+
+adb shell am force-stop com.chucklefish.stardewvalley
+
+adb push bin/ARM64/Debug/net8.0-android/SMAPILoaderDLL.dll "/storage/emulated/0/Android/data/com.chucklefish.stardewvalley/files/SMAPILoaderDLL.dll"
+
+adb shell am start com.chucklefish.stardewvalley/.MainActivity
+
