003290474b582dc6553ae6d942f45ee50233716d
NRTnarathip <61742272+NRTnarathip@users.noreply.github.com>
2024-11-27 20:51:33 +0700

    WIP: Launch EntryGame with TaskTool

--- SMAPIGameLoader/EntryGame.cs
+++ SMAPIGameLoader/EntryGame.cs
@@ -7,27 +7,27 @@ using Android.Widget;
 using HarmonyLib;
 using System;
 using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace SMAPIGameLoader;
 internal static class EntryGame
 {
     public static void LaunchGameActivity(Activity launcherActivity)
     {
-        TaskTool.Run(() =>
+        TaskTool.Run(launcherActivity, () =>
         {
+            ////debug
+            //ToastNotifyTool.Notify("Start sleep 8 sec");
+            //Thread.Sleep(8000);
+            //ToastNotifyTool.Notify("Done sleep.");
+
             LaunchGameActivityInternal(launcherActivity);
         });
     }
 
     static void LaunchGameActivityInternal(Activity launcherActivity)
     {
-#if DEBUG
-        ToastNotifyTool.Notify("Error can't start game on Debug Mode");
-        return;
-#else
-        Console.WriteLine("App is in Release mode");
-#endif
-
         ToastNotifyTool.Notify("Starting Game..");
         //check game it's can launch with version
         if (StardewApkTool.IsGameVersionSupport == false)
@@ -39,17 +39,26 @@ internal static class EntryGame
         try
         {
             //clone game assets
+            TaskTool.AddNewLine("Try clone game assets");
             GameAssetManager.VerifyAssets();
+            TaskTool.AddNewLine("Done verify asset");
             GameAssemblyManager.VerifyAssemblies();
+            TaskTool.AddNewLine("Done verify assemblies");
 
             //Load MonoGame.Framework.dll into reference
             GameAssemblyManager.LoadAssembly(GameAssemblyManager.MonoGameFrameworkDllFileName);
 
             //patch rewrite StardewValley.dll & Load
+            TaskTool.AddNewLine("Try rewrite StardewValley.dll");
             var stardewDllFilePath = GameAssemblyManager.StardewValleyFilePath;
             StardewGameRewriter.Rewrite(stardewDllFilePath, out var isRewrite);
+            TaskTool.AddNewLine("Done rewriter");
             Assembly.LoadFrom(stardewDllFilePath);
 
+#if DEBUG
+            ToastNotifyTool.Notify("Error can't start game on Debug Mode");
+            return;
+#endif
 
             var intent = new Intent(launcherActivity, typeof(SMAPIActivity));
             launcherActivity.StartActivity(intent);
--- SMAPIGameLoader/Tool/TaskTool.cs
+++ SMAPIGameLoader/Tool/TaskTool.cs
@@ -1,22 +1,124 @@
-﻿using System;
+﻿using Android.App;
+using Android.Views;
+using Android.Widget;
+using System;
 using System.Threading.Tasks;
+using Xamarin.Essentials;
 
 namespace SMAPIGameLoader;
 
 internal static class TaskTool
 {
     public static Task LastTask { get; private set; }
-    public static void Run(Action func)
+    public static bool IsBusy => LastTask?.IsCompleted == false;
+    public static Activity LastActivity { get; private set; }
+
+    public static void Run(Activity activity, Action func)
     {
-        if (LastTask?.IsCompleted == false)
+        if (IsBusy)
         {
             Console.WriteLine("Warn!!, current task it running, please wait");
             return;
         }
 
+        //ready
+        LastActivity = activity;
         LastTask = Task.Run(() =>
         {
+            ShowBusyDialog();
             func();
+            CloseBusyDialog();
         });
     }
+
+    static void CloseBusyDialog()
+    {
+        if (MainThread.IsMainThread == false)
+        {
+            RunMainThread(() =>
+            {
+                CloseBusyDialog();
+            });
+            return;
+        }
+
+        if (busyDialog == null)
+            return;
+
+        busyDialog.Dismiss();
+        busyDialog = null;
+    }
+    static AlertDialog busyDialog;
+    static string busyMessage;
+    static void RunMainThread(Action func)
+    {
+        bool doneMainThread = false;
+        if (MainThread.IsMainThread == false)
+        {
+            MainThread.BeginInvokeOnMainThread(() =>
+            {
+                func();
+                doneMainThread = true;
+            });
+        }
+        else
+        {
+            func();
+            doneMainThread = true;
+        }
+
+        while (!doneMainThread)
+        {
+
+        }
+    }
+    static void SetMessage(string msg)
+    {
+        Console.WriteLine("try set msg: " + msg);
+        if (MainThread.IsMainThread == false)
+        {
+            RunMainThread(() =>
+            {
+                SetMessage(msg);
+            });
+            return;
+        }
+
+        if (busyDialog is not null)
+        {
+            busyMessage = msg;
+            busyDialog.SetMessage(msg);
+            Console.WriteLine("done set msg: " + msg);
+        }
+    }
+    public static void AddNewLine(string msg)
+    {
+        RunMainThread(() =>
+        {
+            SetMessage(busyMessage + "\n" + msg);
+        });
+    }
+    static void ShowBusyDialog()
+    {
+        if (MainThread.IsMainThread == false)
+        {
+            MainThread.BeginInvokeOnMainThread(() =>
+            {
+                ShowBusyDialog();
+            });
+            return;
+        }
+
+        CloseBusyDialog();
+
+        Console.WriteLine("try set busy dialog");
+        var dialogBuilder = new AlertDialog.Builder(LastActivity);
+        busyDialog = dialogBuilder.Create();
+        busyDialog.SetTitle("Busy Task");
+        SetMessage("please wait...");
+        busyDialog.SetCancelable(false);
+        busyDialog.Show();
+        Console.WriteLine("done set busy dialog");
+    }
+
 }
