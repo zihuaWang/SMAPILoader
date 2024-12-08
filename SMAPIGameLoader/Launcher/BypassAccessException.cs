3973fa1a419020adf4551b1d50d5a990ca75bd11
NRTnarathip <61742272+NRTnarathip@users.noreply.github.com>
2024-12-09 02:11:12 +0700

    WIP: Bypass Access Exception

--- SMAPIGameLoader/Launcher/BypassAccessException.cs
+++ SMAPIGameLoader/Launcher/BypassAccessException.cs
@@ -1,13 +1,22 @@
-﻿using System;
+﻿using MonoGame.Framework.Utilities;
+using Octokit;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+using static Android.Net.Wifi.WifiEnterpriseConfig;
 
 namespace SMAPIGameLoader.Launcher;
 
 internal static class BypassAccessException
 {
+    [DllImport("libdl.so", CallingConvention = CallingConvention.Cdecl)]
+    public static extern IntPtr dlsym(IntPtr handle, string symbol);
+
     public static void Apply()
     {
         Console.WriteLine("Try Apply BypassAccessException");
@@ -21,9 +30,91 @@ internal static class BypassAccessException
         }
         Console.WriteLine("Successfully BypassAccessException");
     }
+    private const int PROT_READ = 0x1;
+    private const int PROT_WRITE = 0x2;
+    private const int PROT_EXEC = 0x4;
+
+    [DllImport("libc.so", SetLastError = true)]
+    private static extern int mprotect(IntPtr addr, UIntPtr len, int prot);
+
 
     static void ApplyInternal()
     {
+        //var libHandle = dlopen("libmonosgen-2.0.so", 0x1);
+        var libHandle = FuncLoader.LoadLibrary("libmonosgen-2.0.so");
 
+        IntPtr mono_method_can_access_field = dlsym(libHandle, "mono_method_can_access_field");
+
+        Console.WriteLine("Start Patch mono_method_can_access_field");
+        unsafe
+        {
+            //x64
+            IntPtr targetAddress = mono_method_can_access_field + 0x132;
+            byte[] patchBytes = [
+                //0xB8, 0x01, 0x00, 0x00, 0x00, // MOV EAX, 1
+                //0x48, 0x83, 0xC4, 0x58,       // ADD RSP, 0x58
+                //0x5B,                         // POP RBX
+                //0x41, 0x5C,                   // POP R12
+                //0x41, 0x5D,                   // POP R13
+                //0x41, 0x5E,                   // POP R14
+                //0x41, 0x5F,                   // POP R15
+                //0x5D,                         // POP RBP
+                //0xC3                          // RET
+
+                //original
+                0x44, 0x89 ,0xf0,             // MOV EAX, R14D
+                0x48, 0x83, 0xC4, 0x58,       // ADD RSP, 0x58
+                0x5B,                         // POP RBX
+                0x41, 0x5C,                   // POP R12
+                0x41, 0x5D,                   // POP R13
+                0x41, 0x5E,                   // POP R14
+                0x41, 0x5F,                   // POP R15
+                0x5D,                         // POP RBP
+                0xCC                          // RET
+            ];            //add offset into ret
+            patchBytes = Enumerable.Repeat((byte)0xCC, patchBytes.Length).ToArray();
+
+            //Patch(targetAddress, patchBytes);
+            Patch(mono_method_can_access_field, Enumerable.Repeat((byte)0xCC, 0x132).ToArray());
+        }
+        Console.WriteLine("After patch");
+        DumpMemory(mono_method_can_access_field);
+    }
+    static void Patch(IntPtr targetAddress, byte[] patchBytes)
+    {
+        var pageAddress = AlignToPageSize(targetAddress);
+        var pageSize = Environment.SystemPageSize;
+        int protect = mprotect(pageAddress, (uint)pageSize, PROT_EXEC | PROT_READ | PROT_WRITE);
+        if (protect != 0)
+        {
+            Console.WriteLine("error can't set protect memory at address: " + pageAddress.ToString("X"));
+            return;
+        }
+        Marshal.Copy(patchBytes, 0, targetAddress, patchBytes.Length);
+        Console.WriteLine("done patch bytes at address: " + targetAddress.ToString("X"));
+    }
+    static IntPtr AlignToPageSize(IntPtr address)
+    {
+        long pageSize = Environment.SystemPageSize;
+        return new IntPtr(address.ToInt64() & ~(pageSize - 1));
+    }
+
+    static void DumpMemory(nint addressToDump, int dumpLength = 512)
+    {
+        Console.WriteLine("Dump address: " + addressToDump.ToString("X"));
+        nint startAddress = addressToDump;
+        byte[] buffer = new byte[dumpLength];
+        Marshal.Copy(startAddress, buffer, 0, dumpLength);
+
+        // Print the memory in hex
+        for (int i = 0; i < dumpLength; i++)
+        {
+            if (i % 32 == 0)
+            {
+                Console.WriteLine();
+            }
+            Console.Write($"{buffer[i]:X2} ");
+        }
+        Console.WriteLine();
     }
 }
