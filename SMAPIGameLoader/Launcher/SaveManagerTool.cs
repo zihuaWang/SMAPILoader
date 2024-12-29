c3331fe7ff510ed3e39baffcaeaf05fa6b61227e
NRTnarathip <61742272+NRTnarathip@users.noreply.github.com>
2024-12-29 10:29:32 +0700

    WIP: Save Manager

---
+++ SMAPIGameLoader/Launcher/SaveManagerTool.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+
+namespace SMAPIGameLoader.Launcher;
+
+internal static class SaveManager
+{
+    public static void OnClickImportSaveZip()
+    {
+
+    }
+    public static bool ImportSaveZip(FileResult pick)
+    {
+        if (pick.FileName.EndsWith(".zip"))
+            return false;
+
+
+        return true;
+    }
+}
