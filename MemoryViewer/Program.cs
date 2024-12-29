3b56e75aa023dc21f019deb274cf219d9d3604ea
NRTnarathip <61742272+NRTnarathip@users.noreply.github.com>
2024-12-30 00:58:08 +0700

    New Project: Memory Viewer Android

---
+++ MemoryViewer/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Diagnostics;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        while (true)
+        {
+            Thread.Sleep(500);
+
+
+            Process process = new Process();
+            process.StartInfo.FileName = "adb";
+            process.StartInfo.Arguments = "shell cat /proc/meminfo";
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.CreateNoWindow = true;
+
+            Clear();
+            process.Start();
+            string output = process.StandardOutput.ReadToEnd();
+            process.WaitForExit();
+
+            string[] lines = output.Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+            var MemTotal = lines[0];
+            var MemFree = lines[1];
+            var MemAvailable = lines[2];
+            Log(MemTotal);
+            Log(MemFree);
+            Log(MemAvailable);
+        }
+    }
+    static void Log(string lineData)
+    {
+        var data = lineData.Split(":", StringSplitOptions.TrimEntries);
+        int kb = int.Parse(data[1].Replace("kB", "").Trim());
+        var varName = data[0];
+        Console.WriteLine($"{varName}: {kb / (1024f):F3} MB");
+    }
+    static void Clear()
+    {
+        //Console.Write(new string('\n', Console.WindowHeight));
+        //Console.SetCursorPosition(0, 0);
+        Console.Clear();
+    }
+}
